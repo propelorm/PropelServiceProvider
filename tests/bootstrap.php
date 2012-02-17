f1e67ab1057d241b73ead6937d5eb6f20161b6b3
William DURAND <william.durand1@gmail.com>
2012-02-17 18:33:43 +0100

    Initial commit

---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+$loader = require_once __DIR__.'/../vendor/.composer/autoload.php';
+$loader->register('Propel', __DIR__.'/../src');
+
+require_once __DIR__.'/../vendor/propel/propel1/runtime/lib/Propel.php';
