aaef6f3ecbd6c9cd2d4deba53a3b0a1d7aef4bfd
William DURAND <william.durand1@gmail.com>
2012-05-09 20:09:07 +0200

    Continued refactoring

--- src/Propel/Silex/PropelServiceProvider.php
+++ src/Propel/Silex/PropelServiceProvider.php
@@ -22,16 +22,8 @@ class PropelServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        if (isset($app['propel.path'])) {
-            $propel = $app['propel.path'] . '/Propel.php';
-        } else {
-            if (!is_file($propel = realpath('./vendor/propel/propel1/runtime/lib/Propel.php'))) {
-                $propel = realpath('./../vendor/propel/propel1/runtime/lib/Propel.php');
-            }
-        }
-
-        if (!is_file($propel)) {
-            throw new \InvalidArgumentException('Unable to find Propel, did you set the "propel.path" parameter?');
+        if (!class_exists('Propel')) {
+            require_once $this->guessPropel($app);
         }
 
         $modelPath = null;
@@ -49,34 +41,32 @@ class PropelServiceProvider implements ServiceProviderInterface
             $config = $this->guessConfigFile();
         }
 
-        if (isset($app['propel.internal_autoload']) && true === $app['propel.internal_autoload'] && null !== $modelPath) {
-            set_include_path($modelPath . PATH_SEPARATOR . get_include_path());
-        } elseif (null !== $modelPath) {
-            //model namespaces are subdir of $modelPath directory
-            $dir = new \DirectoryIterator($modelPath);
+        if (null !== $modelPath) {
+            if (isset($app['propel.internal_autoload']) && true === $app['propel.internal_autoload']) {
+                set_include_path($modelPath . PATH_SEPARATOR . get_include_path());
+            } else {
+                //model namespaces are subdir of $modelPath directory
+                $dir = new \DirectoryIterator($modelPath);
 
-            //Unfortunately DirectoryIterator count() method is not always implemented, so we need a boolean
-            //to check if $modelPath dir has at least one subdir, otherwise te model has not yet been generated or
-            //$modelPath contains a wrong value.
-            $built = false;
-            foreach ($dir as $fileInfo) {
-                if ($fileInfo->isDir()) {
-                    if (!$fileInfo->isDot()) {
-                        $built = true;
-                        $app['autoloader']->registerNamespace($fileInfo->getFilename(), $modelPath);
+                //Unfortunately DirectoryIterator count() method is not always implemented, so we need a boolean
+                //to check if $modelPath dir has at least one subdir, otherwise te model has not yet been generated or
+                //$modelPath contains a wrong value.
+                $built = false;
+                foreach ($dir as $fileInfo) {
+                    if ($fileInfo->isDir()) {
+                        if (!$fileInfo->isDot()) {
+                            $built = true;
+                            $app['autoloader']->registerNamespace($fileInfo->getFilename(), $modelPath);
+                        }
                     }
                 }
-            }
 
-            if (!$built) {
-                throw new \InvalidArgumentException(
-                    'The '.$modelPath.' has no subdir. May be "propel.model_path" value is wrong or you didn\'t yet generate your model.'
-                );
+                if (!$built) {
+                    throw new \InvalidArgumentException(
+                        'The '.$modelPath.' has no subdir. May be "propel.model_path" value is wrong or you didn\'t yet generate your model.'
+                    );
+                }
             }
-        }
-
-        if (!class_exists('Propel')) {
-            require_once $propel;
         }
 
         \Propel::init($config);
@@ -103,5 +93,22 @@ class PropelServiceProvider implements ServiceProviderInterface
         }
 
         return $config;
+    }
+
+    public function guessPropel(Application $app)
+    {
+        if (isset($app['propel.path'])) {
+            $propel = $app['propel.path'] . '/Propel.php';
+        } else {
+            if (!is_file($propel = realpath('./vendor/propel/propel1/runtime/lib/Propel.php'))) {
+                $propel = realpath('./../vendor/propel/propel1/runtime/lib/Propel.php');
+            }
+        }
+
+        if (!is_file($propel)) {
+            throw new \InvalidArgumentException('Unable to find Propel, did you set the "propel.path" parameter?');
+        }
+
+        return $propel;
     }
 }
