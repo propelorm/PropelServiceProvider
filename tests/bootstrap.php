e47eb590546ea7de5e838c23b92ecf74cc2a0193
Cristiano Cinotti <cristianocinotti@gmail.com>
2012-06-04 17:31:30 +0200

    Issue #7

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-$loader = require_once __DIR__.'/../vendor/.composer/autoload.php';
+$loader = require_once __DIR__.'/../vendor/autoload.php';
 $loader->register('Propel', __DIR__.'/../src');
 
 require_once __DIR__.'/../vendor/propel/propel1/runtime/lib/Propel.php';
